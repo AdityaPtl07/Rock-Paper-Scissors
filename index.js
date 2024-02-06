89ce06eec0ad5e986ca75967d2b1e1152752f415
Aditya Patel <142483507+AdityaPtl07@users.noreply.github.com>
2024-02-06 23:01:31 +0530

    updates

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-let compScore = 0;
-let userScore = 0;
 let userScorePara = document.querySelector("#userScore");
 let compScorePara = document.querySelector("#compScore");
 let currMode = "light";
 let btn = document.querySelector("#mode");
 let body = document.querySelector("body");
+let heading = document.querySelector("h1");
+let label = document.querySelector("label");
+let text = document.querySelector("#text");
+let text1 = document.querySelector("#text1");
+let reset = document.querySelector("#reset");
+
+reset.addEventListener("click", (userScore, compScore) => {
+  userScore = 0;
+  compScore = 0;
+  userScorePara.innerText = userScore;
+  compScorePara.innerText = compScore;
+  msg.innerText = "Let's Play!";
+  playerChoices.forEach((choice) => {
+    choice.style.opacity = 1;
+  });
+  compChoices.forEach((choice) => {
+    choice.style.opacity = 1;
+  });
+});
 
 btn.addEventListener("click", () => {
   if (currMode === "light") {
     body.classList.add("dark");
     body.classList.remove("light");
+    heading.style.backgroundColor = "#1282a2";
+    text.style.color = "white";
+    text1.style.color = "white";
+    userScorePara.style.color = "white";
+    compScorePara.style.color = "white";
     currMode = "dark";
   } else {
     body.classList.add("light");
     body.classList.remove("dark");
+    heading.style.backgroundColor = "#001f54";
     currMode = "light";
   }
 });
 
-let choices = document.querySelectorAll(".choice");
+let playerChoices = document.querySelectorAll(".playerChoice");
+let compChoices = document.querySelectorAll(".compChoice");
 let msg = document.querySelector("#msg");
 
 const genCompChoice = () => {
@@ -27,8 +51,22 @@ const genCompChoice = () => {
   return op[num];
 };
 
-const playGame = (userChoice) => {
-  const compChoice = genCompChoice();
+const playGame = (userChoice, compChoice) => {
+  playerChoices.forEach((choice) => {
+    if (choice.getAttribute("id") !== userChoice) {
+      choice.style.opacity = 0.2;
+    } else {
+      choice.style.opacity = 1;
+    }
+  });
+  compChoices.forEach((choice1) => {
+    if (choice1.getAttribute("id") !== compChoice) {
+      choice1.style.opacity = 0.2;
+    } else {
+      choice1.style.opacity = 1;
+    }
+  });
+
   if (userChoice === compChoice) {
     drawGame();
   } else {
@@ -45,25 +83,25 @@ const playGame = (userChoice) => {
 };
 const showWinner = (userWin) => {
   if (userWin === true) {
-    userScore++;
-    userScorePara.innerText = userScore;
+    const userScore = userScorePara.innerText;
+    userScorePara.innerText = Number(userScore) + 1;
     msg.innerText = "You Win!";
-    msg.style.backgroundColor = "green";
   } else {
-    compScore++;
-    compScorePara.innerText = compScore;
+    const compScore = compScorePara.innerText;
+    compScorePara.innerText = Number(compScore) + 1;
     msg.innerText = "You Lose!";
-    msg.style.backgroundColor = "darkred";
   }
 };
+
 const drawGame = () => {
   msg.innerText = "The Game was draw!";
-  msg.style.backgroundColor = "darkblue";
+  msg.style.Color = "white";
 };
 
-choices.forEach((choice) => {
+playerChoices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
-    playGame(userChoice);
+    const compChoice = genCompChoice();
+    playGame(userChoice, compChoice);
   });
 });
